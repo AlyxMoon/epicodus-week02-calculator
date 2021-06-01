5e8948ebd47c12044098ac61bdcdabcc3e08910b
AlyxMoon <allisterkmoon@gmail.com>
2021-06-01 11:08:48 -0700

    add in some temperature conversion for the bonus work
    Co-authored-by: Brad Prettyman <brad.prettyman5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,32 @@ function divide (num1, num2) {
   return num1 / num2;
 }
 
-const num1 = parseInt(prompt('enter first number'));
-const num2 = parseInt(prompt('enter second number'));
+function convertToCelcius (F) {
+  return (F - 32) * 5/9
+}
 
-const resultAdd = add(num1, num2);
-const resultSubtract = subtract(num1, num2);
-const resultMultiply = multiply(num1, num2);
-const resultDivide = divide(num1, num2);
+function convertToFahrenheit (C) {
+  return (C * (9/5)) + 32
+}
 
-alert('Added: ' + resultAdd);
-alert('Subtracted: ' + resultSubtract);
-alert('Multiplied: ' + resultMultiply);
-alert('Divided: ' + resultDivide);
+const F = parseInt(prompt('Enter Fahrenheit: '));
+const C = parseInt(prompt('Enter Celsius: '));
+
+const resultTemperature = convertToCelcius(F);
+const resultFahrenheit = convertToFahrenheit(C);
+
+alert('Celsius: ' + resultTemperature);
+alert('Fahrenheit: ' + resultFahrenheit);
+
+// const num1 = parseInt(prompt('enter first number'));
+// const num2 = parseInt(prompt('enter second number'));
+
+// const resultAdd = add(num1, num2);
+// const resultSubtract = subtract(num1, num2);
+// const resultMultiply = multiply(num1, num2);
+// const resultDivide = divide(num1, num2);
+
+// alert('Added: ' + resultAdd);
+// alert('Subtracted: ' + resultSubtract);
+// alert('Multiplied: ' + resultMultiply);
+// alert('Divided: ' + resultDivide);
